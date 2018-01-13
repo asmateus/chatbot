11f115e5e4d92bb21b597d12e60938d5d70026d9
Andres Simancas Mateus <mateus@uninorte.edu.co>
2018-01-13 10:49:15 -0500

    URL paths are named now

--- chatbot/chat/urls.py
+++ chatbot/chat/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 from . import views
 
+
 urlpatterns = [
-    path('', views.chat),
-    path('chat/', views.chat),
-    path('login/', auth_views.LoginView.as_view(template_name='chat/login.html')),
-    path('logout/', auth_views.LogoutView.as_view()),
-    path('create-user/', views.chat),
-    path('u/<user>/', views.profile),
-    path('u/<user>/profile/', views.profile),
-    path('u/<user>/history/', views.history),
+    path('', views.chat, name='app-chat'),
+    path('chat/', views.chat, name='app-chat'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('create-user/', views.create_user_view, name='create-user'),
+    path('u/<user>/', views.profile, name='u-profile'),
+    path('u/<user>/profile/', views.profile, name='u-profile'),
+    path('u/<user>/profile/edit', views.profile_edit, name='u-profile-edit'),
+    path('u/<user>/history/', views.history, name='u-history'),
 ]
