6896febc68cd1506fa552e6da1820c73981f4766
Andres Simancas Mateus <mateus@uninorte.edu.co>
2018-01-12 19:04:55 -0500

    Created view functions

--- chatbot/chat/views.py
+++ chatbot/chat/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Required user related methods
+
+def login():
+    return HttpResponse('You got to LOGIN page')
+
+
+def logout():
+    return HttpResponse('You got to LOGOUT page')
+
+
+def create_user():
+    return HttpResponse('You got to CREATE USER page')
+
+# Basic API actions
+
+
+def chat():
+    return HttpResponse('You got to CHAT page')
+
+
+def history():
+    return HttpResponse('You got to HISTORY page')
+
+
+def profile():
+    return HttpResponse('You got to PROFILE page')
