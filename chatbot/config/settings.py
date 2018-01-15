95a23cf2534c5876dcd8c30bc712722cc25ee7b0
Andres Simancas Mateus <mateus@uninorte.edu.co>
2018-01-15 11:39:54 -0500

    Turned off debug flag

--- chatbot/config/settings.py
+++ chatbot/config/settings.py
@@ -5,8 +5,8 @@ from config import secrets
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets.DJANGO_SECRET_KEY
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
