693509ac929b4d18b9da50a8e6b10c68f4d2952f
Andres Simancas Mateus <mateus@uninorte.edu.co>
2018-01-15 21:45:19 -0500

    Added tests for MalformedHandling

--- test/bot_test.py
+++ test/bot_test.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 from query import spawner
-from .utils import _RabbitProducer
+from .utils import _RabbitProducer, _RabbitConsumer
 
 
 class ConnectionToRabbit:
@@ -100,6 +100,7 @@ class MessageRedistribution:
         print('Deleting redistribution queue...')
         self.producer.channel.queue_delete(queue='redistribution-testing-suite')
         print('Closing...')
+        self.producer.close()
 
         if result == len(self.QUEUES) * self.N:
             return 0
@@ -110,11 +111,67 @@ class MessageRedistribution:
 
 
 class MalformedQueryHandling:
+    """
+    Test idea:
+            1. Create a simple producer
+            2. Publish 1 malformed queries and 1 good query
+            3. Start consumers
+            4. Check redistribution channel
+            5. Consume from redistribution channel
+            6. Check messages
+    """
+    QUEUES = ['/stock', '/day_range']
+    QUERIES = ['testing-suite|', 'testing-suite|APPL']
+    MSG_MALFORMED = 'Wrong query format.'
+
     def __init__(self):
-        pass
+        self.producer = _RabbitProducer()
 
     def test(self):
+        print('Creating 1 malformed and 1 good query per queue...')
+        for q in self.QUEUES:
+            print(q + ':', end=' ')
+            for m in self.QUERIES:
+                self.producer.post_query(q, m)
+            print(self.check_queue_size(q))
+
+        print('Spawning consumers...')
+        worker_pids = spawner.Spawner.spawn()
+
+        # Wait for workers
+        time.sleep(10)
+
+        # Stop workers
+        for pid in worker_pids:
+            print('Killing consumer', pid)
+            p = sp.Popen(['kill', str(pid)])
+            p.wait()
+
+        # Fetch messages from redistribution queue
+        print('Fetching messages from redistribution-testing-suite')
+
+        total_to_read = self.check_queue_size('redistribution-testing-suite')
+        consumer = _RabbitConsumer('redistribution-testing-suite', total_to_read)
+        consumer.start_consuming()
+
+        mal_sent = 1 * len(self.QUEUES)
+        good_sent = 1 * len(self.QUEUES)
+        malformed_count = consumer.messages.count(self.MSG_MALFORMED)
+        good_count = len(consumer.messages) - malformed_count
+        print('Detected', malformed_count, 'of', mal_sent, 'malformed queries sent.')
+        print('Detected', good_count, 'of', good_sent, 'good queries sent.')
+
+        print('Deleting redistribution queue...')
+        self.producer.channel.queue_delete(queue='redistribution-testing-suite')
+        print('Closing...')
+        self.producer.close()
+
+        if malformed_count == mal_sent and good_count == good_sent:
+            return 0
         return 1
+
+    def check_queue_size(self, q):
+        return self.producer.channel.queue_declare(queue=q).method.message_count
 
 
 class EmptyAPIResponse:
--- test/utils.py
+++ test/utils.py
@@ -1,6 +1,31 @@
 import pika
 
 
+class _RabbitConsumer:
+    def __init__(self, queue, total):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue)
+        self.channel.basic_consume(self.callback,
+                                   queue=queue,
+                                   no_ack=True)
+        self.messages = []
+        self.total = total
+
+    def start_consuming(self):
+        self.channel.start_consuming()
+
+    def callback(self, ch, method, properties, body):
+        self.messages.append(body.decode('utf-8'))
+
+        if len(self.messages) == self.total:
+            self.channel.stop_consuming()
+
+    def make_return_str(self, api_data):
+        raise NotImplementedError
+
+
 class _RabbitProducer:
     def __init__(self):
         self.connection = pika.BlockingConnection(
